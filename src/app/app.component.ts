d0129c39023c8b174e6ae8c719a1ac162fddb2e5
maxonchintren <maxonchintren@mail.ru>
2020-01-19 20:35:46 +0300

    половина верстки + динамическое изменение цен

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'japan-food';
+  public multiplicator: number = 1;
+
+  public menuPosition: string = 'translateX(-92%)'
+
+  public toggleMenu(): void {
+    if (this.menuPosition === 'translateX(-92%)') {
+      this.menuPosition = 'translateX(0px)';
+    } else {
+      this.menuPosition = 'translateX(-92%)'
+    }
+  }
+
+  //Temporary variable
+public price = 14.95
+//___________________
+  public showPrice() {
+    let target = <HTMLSelectElement>event.target;
+    let div = target.parentElement;
+    console.log(div)
+    let shownPrice = div.querySelector('.pricing')
+    // When objects ready remake this.price => this.dishes.....
+    shownPrice.textContent = '$' + String((this.price * parseFloat(target.options[target.selectedIndex].text)).toFixed(2));
+  } 
 }
